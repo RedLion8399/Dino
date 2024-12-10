5593afe1cd133485ef0a82fff6741a3f978455e2
Paul Jonas Dohle <Paul.Jonas@dohle-net.de>
2024-12-10 12:50:52 +0100

    feat(): Innitial commit
    Created basic file tree
    Added structural and doc files

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""Main module
+"""
+from random import randint
+import pygame as pg
+import time as t
+
+
+
+def main():
+    """Main function
+    """
+    raise NotImplementedError("Function main is missing code"())
+
+
+if __name__ == "__main__":
+    main()
