086c64f7972c9e4da8095e77f001f57063fb18c3
Paul Jonas Dohle <Paul.Jonas@dohle-net.de>
2024-12-12 09:19:33 +0100

    feat(main.py): Add basic progamm structure
    Improve main loop
    Structure basic game functions.
    
    Signed-off-by: Paul Jonas Dohle <Paul.Jonas@dohle-net.de>

--- src/main.py
+++ src/main.py
@@ -1,15 +1,54 @@
-"""Main module
+"""The main module contains the main functionality of the programm.
+It is the file, meant to be called directly to playthe game.
+It initializes the grafics as well as controlls the main game functionalitys
+defined in other modules.
 """
-from random import randint
+
+from random import randint  # pylint: disable=unused-import
+import time as t  # pylint: disable=unused-import
+import sys
 import pygame as pg
-import time as t
+
+pg.init()  # pylint: disable=no-member
 
 
+# Initializes the window
+window_scale: list[int] = [800, 600]
+window: pg.Surface = pg.display.set_mode((window_scale[0], window_scale[1]))  # pylint: disable=no-member
+pg.display.set_caption("Dino")
 
-def main():
-    """Main function
+# Initializes the colors
+black: pg.Color = pg.Color(0, 0, 0)
+white: pg.Color = pg.Color(255, 255, 255)
+gray: pg.Color = pg.Color(190, 190, 190)
+
+object_speed: int
+score: int = 0
+
+
+def game_over() -> None:
+    """This function is called when the game is over."""
+
+    sys.exit()
+    pg.quit()  # pylint: disable=no-member
+
+
+def main() -> None:
+    """This is the main function of the programm.
+    It is the place were every functionality is stticked together.
+    It initializes the game, runs it displays changes and checks for kollisiions.
     """
-    raise NotImplementedError("Function main is missing code"())
+
+    while True:
+        get_input()
+        move_objects()
+        update_display()
+
+        if kollision():
+            break
+        count_score()
+
+    game_over()
 
 
 if __name__ == "__main__":
