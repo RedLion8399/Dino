1193cbb60defe26aac0be820743d11d4aac469c8
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-12-19 15:05:02 +0100

    style(): Change pylint warning notation
    Unified all redundant pylinth disable calls in the beginning
    Added pycache directory to .gitignore.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ It is the file, meant to be called directly to playthe game.
 It initializes the grafics as well as controlls the main game functionalitys
 defined in other modules.
 """
+# pylint: disable=no-member
 
 from random import randint  # pylint: disable=unused-import
 import time as t  # pylint: disable=unused-import
@@ -10,12 +11,12 @@ import sys
 import pygame as pg
 from dino import Dino, Status
 
-pg.init()  # pylint: disable=no-member
+pg.init()
 
 
 # Initializes the window
 window_scale: list[int] = [800, 600]
-window: pg.Surface = pg.display.set_mode((window_scale[0], window_scale[1]))  # pylint: disable=no-member
+window: pg.Surface = pg.display.set_mode((window_scale[0], window_scale[1]))
 pg.display.set_caption("Dino")
 
 dino: Dino = Dino()
@@ -35,16 +36,15 @@ def get_input() -> Status | None:
     """This function gets the input from the user.
     It returns the status of the dino.
     """
-
     for event in pg.event.get():
-        if event.type == pg.QUIT:  # pylint: disable=no-member
+        if event.type == pg.QUIT:
             sys.exit()
-        if event.type == pg.KEYDOWN:  # pylint: disable=no-member
+        if event.type == pg.KEYDOWN:
             if event.key in jump_keys:
                 dino.input_status = Status.JUMPING
             if event.key in sneak_keys:
                 dino.input_status = Status.SNEAKING
-        if event.type == pg.KEYUP:  # pylint: disable=no-member
+        if event.type == pg.KEYUP:
             if event.key in sneak_keys:
                 # TODO Add func to property to change status from sneaking to running
                 dino.input_status = Status.RUNNING
@@ -55,7 +55,7 @@ def game_over() -> None:
     """This function is called when the game is over."""
 
     sys.exit()
-    pg.quit()  # pylint: disable=no-member
+    pg.quit()
 
 
 def main() -> None:
