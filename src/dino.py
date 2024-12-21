f20dd5a0738f2887e02c7188507f77a4b3831a98
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-12-21 16:22:39 +0100

    docs(): Add docstrings for documenting porpuse

--- src/dino.py
+++ src/dino.py
@@ -1,8 +1,19 @@
+""" This module contains the behaviour of the dino.
+The dino is the main character of the game and is controlled by the player.
+It can run, jump and sneak.
+classes:
+    Dino: This class contains the behaviour of the dino.
+    Status: This class contains the status of the dino as an enum.
+"""
 from enum import Enum
 
 class Status(Enum):
     """This class contains the status of the dino.
     The status displays the current state of the dino.
+    Atributes:
+        RUNNING: The dino is running.
+        JUMPING: The dino is jumping.
+        SNEAKING: The dino is sneaking.
     """
     RUNNING = 1
     JUMPING = 2
@@ -10,7 +21,20 @@ class Status(Enum):
 
 
 class Dino:
-    """This class contains the behaviour of the dino."""
+    """This class contains the behaviour of the dino.
+    The dino is the main character of the game and is controlled by the player.
+    It can run, jump and sneak.
+    Atributes:
+        position: The vertical position of the dino.
+        status: The status of the dino as an enum of the Status class.
+
+    Methods:
+        run: sets the dino parameters to running state.
+        jump: sets the dino parameters to jumping state.
+        sneak: sets the dino parameters to sneaking state.
+        update: calls the specific method for the current state of the dino.
+        check_collision: checks if the dino collides with an object.
+    """
     def __init__(self) -> None:
         self.position: int = 20
         self.status: Status = Status.RUNNING
