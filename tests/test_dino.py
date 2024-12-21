313f031bbb282f0201209896c1c9c6ffc40d73de
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-12-21 18:38:24 +0100

    test(dino): Add unit tests for process_input method in Dino class

--- tests/test_dino.py
+++ tests/test_dino.py
@@ -1,6 +1,9 @@
 """This module contains the tests for the dino module."""
+# pylint: disable=no-member
+# pylint: disable=missing-function-docstring
 
 import unittest
+import pygame as pg
 from dino import Dino, Status
 
 class TestDino(unittest.TestCase):
@@ -11,6 +14,45 @@ class TestDino(unittest.TestCase):
         dino: Dino = Dino()
         self.assertEqual(dino.position, 20)
         self.assertEqual(dino.status, Status.RUNNING)
+
+    # The following functions tests the process_input method of the Dino class.
+    def test_process_input_from_running_to_jumping(self) -> None:
+        dino: Dino = Dino()
+        event: pg.event.Event = pg.event.Event(pg.KEYDOWN, key=pg.K_UP)
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.JUMPING)
+
+        dino.status = Status.RUNNING
+        event: pg.event.Event = pg.event.Event(pg.KEYDOWN, key=pg.K_SPACE)
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.JUMPING)
+
+    def test_process_input_from_running_to_sneaking(self) -> None:
+        dino: Dino = Dino()
+        event: pg.event.Event = pg.event.Event(pg.KEYDOWN, key=pg.K_DOWN)
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.SNEAKING)
+
+    def test_process_input_from_jumping_to_running(self) -> None:
+        dino: Dino = Dino()
+        event: pg.event.Event = pg.event.Event(pg.KEYUP, key=pg.K_UP)
+        dino.status = Status.JUMPING
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.JUMPING)
+
+    def test_process_input_from_sneaking_to_running(self) -> None:
+        dino: Dino = Dino()
+        event: pg.event.Event = pg.event.Event(pg.KEYUP, key=pg.K_DOWN)
+        dino.status = Status.SNEAKING
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.RUNNING)
+
+    def test_process_input_from_jumping_to_sneaking(self) -> None:
+        dino: Dino = Dino()
+        event: pg.event.Event = pg.event.Event(pg.KEYDOWN, key=pg.K_DOWN)
+        dino.status = Status.JUMPING
+        dino.process_input(event)
+        self.assertEqual(dino.status, Status.JUMPING)
 
 
 class TestStatus(unittest.TestCase):
