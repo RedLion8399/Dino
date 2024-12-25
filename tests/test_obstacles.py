476839329caf1dd534676f19fb13945e334aebdc
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-12-25 17:38:32 +0100

    test(): Add unit tests for Cactus class initialization and image attributes

--- tests/test_obstacles.py
+++ tests/test_obstacles.py
@@ -1,10 +1,31 @@
 """This module contains the tests for the obstacles module."""
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring, no-member
 import unittest
-from obstacles import GameElement
+import pygame as pg
+from obstacles import GameElement, Cactus
 
 class TestGameElement(unittest.TestCase):
     def test_init(self):
         game_element = GameElement(0, 0)
         self.assertEqual(game_element.x_position, 0)
         self.assertEqual(game_element.y_position, 0)
+
+
+class TestCactus(unittest.TestCase):
+    def setUp(self) -> None:
+        pg.init()
+        pg.display.set_mode((800, 600))
+
+    def test_init_position(self):
+        cactus = Cactus(0, 0)
+        self.assertEqual(cactus.x_position, 0)
+        self.assertEqual(cactus.y_position, 0)
+
+    def test_init_images(self):
+        cactus = Cactus(0, 0)
+        self.assertIsInstance(cactus.image_1, tuple)
+        self.assertIsInstance(cactus.image_2, tuple)
+        self.assertIsInstance(cactus.image_1[0], list)
+        self.assertIsInstance(cactus.image_2[0], list)
+        self.assertIsInstance(cactus.image_1[1], pg.Rect)
+        self.assertIsInstance(cactus.image_2[1], pg.Rect)
