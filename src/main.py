b57c43054db62990612c6d194cd72c144859f0a7
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-12-29 12:11:27 +0100

    feat(main.py): Change Window scale
    Move game functionality to main functiona ccording to ggogle style guidlines.

--- src/main.py
+++ src/main.py
@@ -15,43 +15,41 @@ from config import ColorTheme, config
 from counter import Counter
 from dino import Dino
 
-config.color_theme = ColorTheme.LIGHT_GRAY
-config.display_scale = (800, 600)
-config.caption = "Dino"
-config.init_screen()
-
-counter: Counter = Counter()
-dino: Dino = Dino()
-
-# Initializes the colors
-BLACK: Final[pg.Color] = pg.Color(0, 0, 0)
-WHITE: Final[pg.Color] = pg.Color(255, 255, 255)
-GRAY: Final[pg.Color] = pg.Color(190, 190, 190)
-
-
-def get_input() -> None:
-    """This function gets the input from the user.
-    It collects all inputs from the previous frame and
-    processes them by modyfying the game according to the player wishes.
-    """
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-        dino.process_input(event)
-
-
-def game_over() -> None:
-    """This function is called when the game is over."""
-    counter.save_highscore()
-    pg.quit()
-    sys.exit()
-
 
 def main() -> None:
     """This is the main function of the programm.
     It is the place were every functionality is stticked together.
     It initializes the game, runs it displays changes and checks for kollisiions.
     """
+    config.color_theme = ColorTheme.LIGHT_GRAY
+    config.display_scale = (800, 300)
+    config.caption = "Dino"
+    config.init_screen()
+
+    counter: Counter = Counter()
+    dino: Dino = Dino()
+
+    # Initializes the colors
+    BLACK: Final[pg.Color] = pg.Color(0, 0, 0)
+    WHITE: Final[pg.Color] = pg.Color(255, 255, 255)
+    GRAY: Final[pg.Color] = pg.Color(190, 190, 190)
+
+    def get_input() -> None:
+        """This function gets the input from the user.
+        It collects all inputs from the previous frame and
+        processes them by modyfying the game according to the player wishes.
+        """
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            dino.process_input(event)
+
+    def game_over() -> None:
+        """This function is called when the game is over."""
+        counter.save_highscore()
+        pg.quit()
+        sys.exit()
+
     while True:
         get_input()
         counter.count()
