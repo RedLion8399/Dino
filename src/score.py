874eeabe4ffe091ac17d6575d456e3d1ecd86bdb
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-01-08 21:49:08 +0100

    feat(): Add Score class for score management and display
    The score is now displayed in the upper right corner.

---
+++ src/score.py
@@ -0,0 +1,40 @@
+"""The score the player recieves is displayed heren
+
+As long as the player survives in the game he recieves
+score points. The score is displayed on the screen.
+"""
+
+import pygame as pg
+
+from config import config
+from obstacles import GameElement
+from recourses import load_image, seperate_images
+
+
+class Score(GameElement):
+    """The score management and display is implemented here."""
+
+    def __init__(self) -> None:
+        super().__init__(config.display_scale[0], 0)
+        self.OBJECT_SPEED = 0
+
+        self.immages: tuple[list[pg.Surface], pg.Rect, pg.Rect]
+        self.immages = seperate_images(load_image("numbers.png")[0], (12, 1))
+        self.immage, self.position_rect, self.hitbox = self.immages
+
+    def update(self) -> None:
+        # At first the singulat chars are seperated and
+        # filled up to a total of five using zeros
+        sore: int = self.counter.score
+        digits: list[int] = [int(digit) for digit in str(sore)]
+        digits.reverse()
+        while len(digits) < 5:
+            digits.append(0)
+        digits.reverse()
+
+        # The immages are then filled up with the digits
+        # and the immages are blit to the screen
+        for i, digit in enumerate(digits):
+            self.current_image = self.immage[digit]
+            self.position_rect.x = config.display_scale[0] - 125 + i * 25
+            config.window.blit(self.current_image, self.position_rect)
